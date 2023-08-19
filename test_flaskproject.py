6fb23b9fe4e32cbd85e4d0b89770b729bc647217
Akhilbmsb <akhiill540@gmail.com>
2023-08-19 11:42:55 +0530

    added flaskprojest and testfile

---
+++ test_flaskproject.py
@@ -0,0 +1,21 @@
+from flaskproject import add,sub,mult,div,sqrt
+
+def test_add():
+    result=add(-2,-3)
+    assert result == -5
+    
+def test_sub():
+    result=sub(-2,-3)
+    assert result == 1
+    
+def test_mult():
+    result=mult(2,3)
+    assert result == 6
+    
+def test_div():
+    result=div(2,0)
+    assert result == "infinity"
+    
+def test_sqroot():
+    result=sqrt(4)
+    assert result == 2
